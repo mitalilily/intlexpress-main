cde21c3987d6ee687739c78c9482a527416769c5
mitalilily <mitalilily8877@gmail.com>
2026-06-17 11:31:40 +0530

    Fix backend upload typings and runtime deps

---
+++ backend/src/types/express.d.ts
@@ -0,0 +1,12 @@
+import 'multer'
+
+declare global {
+  namespace Express {
+    interface Request {
+      file?: Express.Multer.File
+      files?: Express.Multer.File[] | { [fieldname: string]: Express.Multer.File[] }
+    }
+  }
+}
+
+export {}
